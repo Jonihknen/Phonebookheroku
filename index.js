a48abab2bdf97de93804fd7304b03438ebd4c6d2
Joni <jonihknen@gmail.com>
2022-04-29 22:47:15 +0300

    front added

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let persons = [
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('postinfo', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postinfo'))
