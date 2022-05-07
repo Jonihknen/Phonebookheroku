213c8debfdd1b45444ce1f9e05913a27484ff254
Joni <jonihknen@gmail.com>
2022-05-07 19:11:43 +0300

    part 3 done

---
+++ build/precache-manifest.4def37d043298197cb6a5be45937d3b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8c058c39ef79065414",
+    "url": "/static/css/main.6e5b9e7f.chunk.css"
+  },
+  {
+    "revision": "aa8c058c39ef79065414",
+    "url": "/static/js/main.aa8c058c.chunk.js"
+  },
+  {
+    "revision": "a6bacfbe9a4f9b2fbe1b",
+    "url": "/static/js/1.a6bacfbe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b68afbe1a99cdae432820263e74d427c",
+    "url": "/index.html"
+  }
+];
